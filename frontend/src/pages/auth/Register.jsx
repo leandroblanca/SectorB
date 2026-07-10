d4fbeff6e1051bcd9120aeda98d8d6ced6390d14
leandroblanca <ezepiolablank@gmail.com>
2026-07-10 17:55:23 -0300

    feat: crear componente de registro con formulario completo

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,6 +1,46 @@
+import "../auth/Auth.css"
 function Register(params) {
     return (
-        <h1>Register</h1>
+        <main className="login-page">
+                    <div className="login-container">
+                        <h1>SectorB</h1>
+                        <h2>Bienvenido</h2>
+        
+                        <p>Introduce tu gmail y contraseña para entrar</p>
+        
+                        <form>
+                            <div>
+                                <label> Nombre </label>
+                                <input type="text"
+                                placeholder="leo" />
+                            </div>
+                            <div>
+                                <label> Apellido </label>
+                                <input type="text"
+                                placeholder="Blanca" />
+                            </div>
+                            <div>
+                                <label> Email </label>
+                                <input type="text"
+                                placeholder="leoblank@gmail.com" />
+                            </div>
+                            <div>
+                                <label>Contraseña</label>
+                                <input type="password"
+                                 placeholder="*******" />
+                            </div>
+                            <div>
+                                <label>Confirmar Contraseña</label>
+                                <input type="password"
+                                 placeholder="*******" />
+                            </div>
+                            <button type="submit">
+                                Register
+                            </button>
+                        </form>
+                        <p>Ya tengo una cuenta?<Link to="/login">Login</Link></p>
+                    </div>
+                </main>
     )
 }
 
