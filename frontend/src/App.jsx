ad0e2a8eaef9308be42354b38f4f22840a11d8ab
leandroblanca <ezepiolablank@gmail.com>
2026-07-28 12:44:08 -0300

    feat: agregar ruta para el componente Home en la aplicación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
     <ToastContainer
         position="top-right"
