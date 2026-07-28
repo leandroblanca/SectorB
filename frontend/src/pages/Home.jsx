f7ac4ec50045953edee57e1368a516d8b84dd454
leandroblanca <ezepiolablank@gmail.com>
2026-07-28 12:40:53 -0300

    feat: reestructurar componente Home para incluir NavBar y Hero

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,14 @@
-<h1> Bienvenido a SectorB </h1>
+import Hero from "../components/hero/Hero";
+import NavBar from "../components/layout/Navbar";
+
+function Home(params) {
+    return (
+        <>
+        <NavBar/>
+        <Hero/>
+        <h1> Bienvenido a SectorB </h1>
+        </>
+    )
+}
+
+export default Home;
